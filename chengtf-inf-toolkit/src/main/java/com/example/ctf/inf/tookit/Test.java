c0192fd18361770a7471237d590513089d211a06
chengtf94 <chengtf94@qq.com>
2024-03-23 16:33:02 +0800

    init

---
+++ chengtf-inf-toolkit/src/main/java/com/example/ctf/inf/tookit/Test.java
@@ -0,0 +1,14 @@
+package com.example.ctf.inf.tookit;
+
+/**
+ * @description: TODO
+ * @author: chengtf
+ * @date: 2024/3/23 16:32
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
